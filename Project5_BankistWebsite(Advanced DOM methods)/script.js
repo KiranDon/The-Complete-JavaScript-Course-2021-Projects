4ed57a01d4e65824e4b557be5893ec0ba74b0551
KiranDon <ushakiranmelasthri123@gmail.com>
2021-04-04 11:33:42 +0530

    Working on modal window

---
+++ Project5_BankistWebsite(Advanced DOM methods)/script.js
@@ -0,0 +1,33 @@
+// script for modal starts here
+const openModalButtons = document.querySelectorAll('.openModal');
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeButton = document.querySelector('.closeButton');
+
+const openModal = function(e){
+    e.preventDefault();
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+}
+
+const closeModal = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+}
+
+for(let i=0; i<openModalButtons.length; i++)
+{
+    openModalButtons[i].addEventListener('click', openModal);
+}
+
+closeButton.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function(e){
+    if(e.keyCode===27 && !modal.classList.contains('hidden'))
+    {
+        closeModal();
+    }
+});
+// script for modal Ends here
