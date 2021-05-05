7ff9200d7c6eb16d11f4375dee292a5de5d04d41
KiranDon <ushakiranmelasthri123@gmail.com>
2021-05-05 19:37:48 +0530

    Learning npm

---
+++ Project8_IntroductionToNPM(not an actual project)/script.js
@@ -0,0 +1,8 @@
+// 'use strict';
+// import {isEven} from './node_modules/is-even/index.js'
+var isEven = require('is-even');
+console.log(isEven(2));
+console.log(isEven(6));
+console.log(isEven(23));
+console.log(isEven(3));
+console.log(isEven(100));
