ccfe5669a96e7bee289ce4345249d5e6ba75af7d
KiranDon <ushakiranmelasthri123@gmail.com>
2021-05-10 19:46:30 +0530

    finished forkify frontend

--- Project8_IntroductionToNPM(not an actual project)/script.js
+++ Project8_IntroductionToNPM(not an actual project)/script.js
@@ -1,8 +1,14 @@
 // 'use strict';
 // import {isEven} from './node_modules/is-even/index.js'
-var isEven = require('is-even');
-console.log(isEven(2));
-console.log(isEven(6));
-console.log(isEven(23));
-console.log(isEven(3));
-console.log(isEven(100));
+// var isEven = require('is-even');
+// console.log(isEven(2));
+// console.log(isEven(6));
+// console.log(isEven(23));
+// console.log(isEven(3));
+// console.log(isEven(100));
+
+// import isPrime from './node_modules/prime-fun/'index.js'
+// import primes from './node_modules/prime-fun/index.js';
+// console.log(primes.isPrime(19))
+
+import {isEven} from './node_modules/is-even/index.js'
